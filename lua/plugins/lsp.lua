7252bb297c525d30e613764d4892f915bd925bc8
infcode <infcode@umich.edu>
2024-06-08 18:23:23 +0800

    feat: add lazydev for smooth config development

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -104,5 +104,17 @@ return {
 	},
 	{
 		"williamboman/mason.nvim",
-	}
+	},
+	{
+		"folke/lazydev.nvim",
+		ft = "lua", -- only load on lua files
+		opts = {
+			library = {
+				-- See the configuration section for more details
+				-- Load luvit types when the `vim.uv` word is found
+				{ path = "luvit-meta/library", words = { "vim%.uv" } },
+			},
+		},
+	},
+	{ "Bilal2453/luvit-meta", lazy = true }, -- optional `vim.uv` typings
 }
