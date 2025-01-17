7a58b73a6c4a91e32cb5daf2a6075b2d9d367900
infcode <infcode@163.com>
2025-01-17 14:37:34 -0500

    docs: add comment to diagnostic

--- lua/diagnostic.lua
+++ lua/diagnostic.lua
@@ -1,6 +1,12 @@
 local M = {}
 
 function M.set_float_autocmd()
+	-- This specifies how long nvim will wait for new inputs before writing
+	-- swap file to the disk, and also the wait time before CursorHold being
+	-- triggered.
+	-- The default updatetime is 4000, which is too long to wait for the diagnostic
+	-- :h CursorHold says in the future there might be a separate config for the
+	-- wait time, so we won't need to mess up with the swap files.
 	vim.o.updatetime = 250
 	local group = vim.api.nvim_create_augroup("diagnostic", {})
 	vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
