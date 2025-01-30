32ddfb55568cba4a26e81dc5c7eaaf75fcffdb42
infcode <infcode@163.com>
2025-01-29 19:39:37 -0500

    feat: add neo-tree

---
+++ lua/plugins/tools.lua
@@ -0,0 +1,24 @@
+return {
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"MunifTanjim/nui.nvim",
+		-- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
+	},
+	opts = {
+		window = {
+			mappings = {
+				["P"] = {
+					"toggle_preview",
+					config = {
+						use_float = false,
+						-- use_image_nvim = true,
+						title = 'Preview',
+					},
+				},
+			}
+		}
+	}
+}
