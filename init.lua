5c4568e123b8f87733df85a3897804538f6b4247
infcode <infcode@163.com>
2025-02-12 01:35:12 -0500

    refactor: move global neovim-native configs into core/

--- init.lua
+++ init.lua
@@ -2,11 +2,9 @@ if vim.g.vscode then
     -- VSCode vim extension
 else
     -- normal neovim config
-    require("options")
-    require("mappings")
+    require("core")
     require("utils")
     require("diagnostic").setup()
     require("deps")
-    require("autocmd")
     require("terminal").setup()
 end
