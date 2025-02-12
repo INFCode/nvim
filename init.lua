cb48c5881ce19730051ea77ba7c297fd8f43d9e7
infcode <infcode@163.com>
2025-02-12 01:17:10 -0500

    refactor: use require instead of _G for utils

--- init.lua
+++ init.lua
@@ -1,12 +1,12 @@
 if vim.g.vscode then
-	-- VSCode vim extension
+    -- VSCode vim extension
 else
-	-- normal neovim config
-	require("options")
-	require("mappings")
-	require("utils") -- this defines the _G.Utils, so it must go first
-	require("diagnostic").setup()
-	require("deps")
-	require("autocmd")
-	require("terminal").setup()
+    -- normal neovim config
+    require("options")
+    require("mappings")
+    require("utils")
+    require("diagnostic").setup()
+    require("deps")
+    require("autocmd")
+    require("terminal").setup()
 end
--- lua/utils.lua
+++ lua/utils.lua
@@ -2,21 +2,21 @@ local M = {}
 
 -- some dirty functions that hacks lazy.nvim to get internal states
 function M.is_loaded(name)
-	local Config = require("lazy.core.config")
-	return Config.plugins[name] and Config.plugins[name]._.loaded
+    local Config = require("lazy.core.config")
+    return Config.plugins[name] and Config.plugins[name]._.loaded
 end
 
 function M.get_plugin(name)
-	return require("lazy.core.config").spec.plugins[name]
+    return require("lazy.core.config").spec.plugins[name]
 end
 
 function M.opts(name)
-	local plugin = M.get_plugin(name)
-	if not plugin then
-		return {}
-	end
-	local Plugin = require("lazy.core.plugin")
-	return Plugin.values(plugin, "opts", false)
+    local plugin = M.get_plugin(name)
+    if not plugin then
+        return {}
+    end
+    local Plugin = require("lazy.core.plugin")
+    return Plugin.values(plugin, "opts", false)
 end
 
-_G.Utils = M
+return M
