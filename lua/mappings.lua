33c38ca8c21a3ea57ecccd2666af3559040d32e9
infcode <infcode@163.com>
2025-02-11 20:36:53 -0500

    feat: enriched the mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,6 +1,25 @@
 -- key mapping
 local map = vim.keymap.set
-vim.g.mapleader = "'"
 
-map("i", "jk", "<esc>")
-map("n", "<leader>h", "<cmd>nohl<cr>")
+-- leader key
+vim.g.mapleader = "'"
+vim.g.maplocalleader = "'"
+
+local default_opts = { noremap = true, silent = true } -- default options for the mappings
+map("i", "jk", "<esc>", default_opts)                  -- Use `jk` to exit insert mode
+map("n", "<leader>h", "<cmd>nohl<cr>", default_opts)   -- use leader-h to stop highlighing
+map("n", "x", '"_x', default_opts)                     -- delete single character without polluting the register
+
+-- Moving between tabs
+map('n', '<Tab>', ':bnext<CR>', default_opts)
+map('n', '<S-Tab>', ':bprevious<CR>', default_opts)
+
+-- Window management
+map('n', '<leader>v', '<C-w>v', default_opts) -- split window vertically
+map('n', '<leader>h', '<C-w>s', default_opts) -- split window horizontally
+
+-- Resize splits with Ctrl-arrows
+map('n', '<C-Up>', ':resize -2<CR>', default_opts)
+map('n', '<C-Down>', ':resize +2<CR>', default_opts)
+map('n', '<C-Left>', ':vertical resize -2<CR>', default_opts)
+map('n', '<C-Right>', ':vertical resize +2<CR>', default_opts)
