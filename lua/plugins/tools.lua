d2ab6a9585ff5231d07ecdaf516489aabbedcc2b
infcode <infcode@163.com>
2025-02-11 20:59:28 -0500

    replace mini.icon with nvim-web-devicon

--- lua/plugins/tools.lua
+++ lua/plugins/tools.lua
@@ -1,51 +1,52 @@
 return {
-	{
-		"nvim-neo-tree/neo-tree.nvim",
-		branch = "v3.x",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-			"MunifTanjim/nui.nvim",
-			-- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
-		},
-		opts = {
-			window = {
-				mappings = {
-					["P"] = {
-						"toggle_preview",
-						config = {
-							use_float = false,
-							-- use_image_nvim = true,
-							title = 'Preview',
-						},
-					},
-				}
-			}
-		}
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+            "MunifTanjim/nui.nvim",
+            -- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
+        },
+        opts = {
+            close_if_last_window = true,
+            window = {
+                mappings = {
+                    ["P"] = {
+                        "toggle_preview",
+                        config = {
+                            use_float = false,
+                            -- use_image_nvim = true,
+                            title = 'Preview',
+                        },
+                    },
+                }
+            }
+        }
 
-	},
-	{
-		"preservim/vim-pencil",
-		init = function()
-			vim.g["pencil#wrapModeDefault"] = "soft"
-		end,
-	},
-	{
-		"folke/which-key.nvim",
-		event = "VeryLazy",
-		opts = {
-			-- your configuration comes here
-			-- or leave it empty to use the default settings
-			-- refer to the configuration section below
-		},
-		keys = {
-			{
-				"<leader>?",
-				function()
-					require("which-key").show({ global = false })
-				end,
-				desc = "Buffer Local Keymaps (which-key)",
-			},
-		},
-	}
+    },
+    {
+        "preservim/vim-pencil",
+        init = function()
+            vim.g["pencil#wrapModeDefault"] = "soft"
+        end,
+    },
+    {
+        "folke/which-key.nvim",
+        event = "VeryLazy",
+        opts = {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+        },
+        keys = {
+            {
+                "<leader>?",
+                function()
+                    require("which-key").show({ global = false })
+                end,
+                desc = "Buffer Local Keymaps (which-key)",
+            },
+        },
+    }
 }
--- lua/plugins/visual.lua
+++ lua/plugins/visual.lua
@@ -1,83 +1,82 @@
 return {
-	{
-		"loctvl842/monokai-pro.nvim",
-		opts = {
-			filter = "spectrum",
-			background_clear = {
-				"float_win"
-			}
-		},
-		config = function(_, opts)
-			require("monokai-pro").setup(opts)
-			vim.cmd([[colorscheme monokai-pro]])
-		end,
-		priority = 1000
-	},
-	{
-		"lukas-reineke/indent-blankline.nvim",
-		main = "ibl",
-		opts = function()
-			local lighten = require("monokai-pro.color_helper").lighten
+    {
+        "loctvl842/monokai-pro.nvim",
+        opts = {
+            filter = "spectrum",
+            background_clear = {
+                "float_win"
+            }
+        },
+        config = function(_, opts)
+            require("monokai-pro").setup(opts)
+            vim.cmd([[colorscheme monokai-pro]])
+        end,
+        priority = 1000
+    },
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        main = "ibl",
+        opts = function()
+            local lighten = require("monokai-pro.color_helper").lighten
 
-			local function get_fg_color(hl_group)
-				local hl = vim.api.nvim_get_hl(0, { name = hl_group })
+            local function get_fg_color(hl_group)
+                local hl = vim.api.nvim_get_hl(0, { name = hl_group })
 
-				if hl and hl.fg then
-					return string.format("#%06x", hl.fg)
-				else
-					return "NONE"
-				end
-			end
+                if hl and hl.fg then
+                    return string.format("#%06x", hl.fg)
+                else
+                    return "NONE"
+                end
+            end
 
-			local hl_monokai = {}
-			local hl_light_monokai = {}
-			local monokai_hl_group_prefix = "IndentBlankLineIndent"
-			local light_hl_group_prefix = "IndentBlankLineScope"
-			for i = 1, 6 do
-				local indent_color_group = monokai_hl_group_prefix .. i
-				local scope_color_group = light_hl_group_prefix .. i
-				table.insert(hl_monokai, indent_color_group)
-				local hex = get_fg_color(indent_color_group)
-				local lighten_hex = lighten(hex, 60)
-				vim.api.nvim_set_hl(0, scope_color_group, { fg = lighten_hex })
-				table.insert(hl_light_monokai, scope_color_group)
-			end
+            local hl_monokai = {}
+            local hl_light_monokai = {}
+            local monokai_hl_group_prefix = "IndentBlankLineIndent"
+            local light_hl_group_prefix = "IndentBlankLineScope"
+            for i = 1, 6 do
+                local indent_color_group = monokai_hl_group_prefix .. i
+                local scope_color_group = light_hl_group_prefix .. i
+                table.insert(hl_monokai, indent_color_group)
+                local hex = get_fg_color(indent_color_group)
+                local lighten_hex = lighten(hex, 60)
+                vim.api.nvim_set_hl(0, scope_color_group, { fg = lighten_hex })
+                table.insert(hl_light_monokai, scope_color_group)
+            end
 
 
-			return {
-				indent = {
-					highlight = hl_monokai,
-					char = "▏",
-					smart_indent_cap = true,
-				},
-				scope = {
-					highlight = hl_light_monokai,
-					char = '▎',
-				}
-			}
-		end
-	},
-	{
-		"NvChad/nvim-colorizer.lua",
-		event = "BufReadPre",
-		opts = {
-			filetypes = { "*" },
-			user_default_options = {
-				RGB = true, -- #RGB hex codes
-				RRGGBB = true, -- #RRGGBB hex codes
-				RRGGBBAA = true, -- #RRGGBBAA hex codes
-				AARRGGBB = false, -- 0xAARRGGBB hex codes
-				-- Available modes for `mode`: foreground, background,  virtualtext
-				mode = "virtualtext", -- Set the display mode.
-				virtualtext = "■",
-			},
-		},
-	},
-	{
-		"MunifTanjim/nui.nvim"
-	},
-	{
-		'echasnovski/mini.icons',
-		version = false,
-	},
+            return {
+                indent = {
+                    highlight = hl_monokai,
+                    char = "▏",
+                    smart_indent_cap = true,
+                },
+                scope = {
+                    highlight = hl_light_monokai,
+                    char = '▎',
+                }
+            }
+        end
+    },
+    {
+        "NvChad/nvim-colorizer.lua",
+        event = "BufReadPre",
+        opts = {
+            filetypes = { "*" },
+            user_default_options = {
+                RGB = true,           -- #RGB hex codes
+                RRGGBB = true,        -- #RRGGBB hex codes
+                RRGGBBAA = true,      -- #RRGGBBAA hex codes
+                AARRGGBB = false,     -- 0xAARRGGBB hex codes
+                -- Available modes for `mode`: foreground, background,  virtualtext
+                mode = "virtualtext", -- Set the display mode.
+                virtualtext = "■",
+            },
+        },
+    },
+    {
+        "MunifTanjim/nui.nvim"
+    },
+    {
+        "nvim-tree/nvim-web-devicons", opts = {}
+    },
 }
