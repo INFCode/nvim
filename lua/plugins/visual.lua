88912aaf5ecebbcd052a45766d73e80445606421
infcode <infcode@163.com>
2025-02-13 23:39:30 -0500

    feat: add TODO highlighting

--- lua/plugins/visual.lua
+++ lua/plugins/visual.lua
@@ -79,4 +79,12 @@ return {
     {
         "nvim-tree/nvim-web-devicons", opts = {}
     },
+    {
+        "folke/todo-comments.nvim",
+        event = "VimEnter",
+        dependencies = {
+            'nvim-lua/plenary.nvim'
+        },
+        opts = { signs = false }
+    }
 }
